384d638b9e08c384bc26856bd02f1e3457d195d7
Azul <diego.kfu@hotmail.com>
2021-02-27 06:56:14 -0300

    azul.js

--- bat/debug.bat
+++ bat/debug.bat
@@ -1,5 +1,5 @@
 @echo off
 cd ..
 echo Starting in Chrome DebugMode..
-node --max-old-space-size=4096 --expose-gc --inspect=9222 blue.js
+node --max-old-space-size=4096 --expose-gc --inspect=9222 azul.js
 pause
